10c81f4de3557c19690f3a453114cfa71d0e8e4a
VITOR ISIDIO FEITOSA DA SILVA <25203658@dsn127m1246729.cfp127aluno.local>
2026-02-24 11:26:57 -0300

    fazendo o botão apagar

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict'
+
+function adicionarProduto(){
+    const lista = document.getElementById('lista')
+    const produto = document.getElementById('produto')
+
+    const span = document.createElement('span')
+    span.textContent = produto.value
+    span.className = 'flex flex col bg-yellow-100 px-8 py-2'
+    
+    if(produto.value == ""){
+        console.log('erro')
+    }else{
+    lista.appendChild(span)
+    }
+
+}
+
+function apagarProduto(){
+    const lista = document.getElementById('lista')
+}
