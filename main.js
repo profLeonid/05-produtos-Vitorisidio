319eccf14252d67ad24cc7123bd038a53f6e449c
Vitorisidio <vitorisidiosenai@gmail.com>
2026-03-08 17:57:31 -0300

    implementação da tabela

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
 'use strict'
 
-function adicionarProduto(){
+function adicionarProduto() {
     const lista = document.getElementById('lista')
+    const codigo = document.getElementById('codigo')
     const produto = document.getElementById('produto')
+    const quantidade = document.getElementById('quantidade')
+
 
     const span = document.createElement('span')
-    span.textContent = produto.value
+    span.textContent = `Código: ${codigo.value} | Produto: ${produto.value} | Quantidade: ${quantidade.value}`
     span.className = 'flex flex col bg-yellow-100 px-8 py-2'
-    
-    if(produto.value == ""){
+
+    if (codigo.value == "" || produto.value == "" || quantidade.value == "") {
         console.log('erro')
-    }else{
-    lista.appendChild(span)
+    } else if (isNaN(codigo.value) || isNaN(quantidade.value)) {
+        console.log('erro')
+    } else {
+        const linha = document.createElement('tr')
+        linha.innerHTML = `<td>${codigo.value}</td> <td>${produto.value}</td> <td>${quantidade.value}</td>`
+        lista.appendChild(linha)
+
+        codigo.value = ""
+        produto.value = ""
+        quantidade.value = ""
     }
 
 }
 
-function apagarProduto(){
+function apagarProduto() {
     const lista = document.getElementById('lista')
+    if (lista.lastElementChild) {
+        lista.lastElementChild.remove()
+    }
 }
